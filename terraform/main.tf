049499ba2e26f4d150d1df49c9c20ec2443f98a9
OKECHUKWU STANISLAUS ORIMADIKE <orimadikeokechukwu@gmail.com>
2023-03-20 22:41:53 +0000

    Piping the project

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "kube_cluster"
+  cluster_name = "altschool_exam"
 }
 
 resource "random_string" "suffix" {
@@ -27,7 +27,7 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.19.0"
 
-  name = "kube-vpc"
+  name = "altschool-vpc"
 
   cidr = "10.0.0.0/16"
   azs  = slice(data.aws_availability_zones.available.names, 0, 3)
